c184413495ef1e5a7adea6a94732cdda98fb8e73
Guy Barros <guy@hashicorp.com>
2020-06-17 22:39:08 -0300

    initial working code

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # python-vault-basic
-this is a simple python example of loging into Vault and reading a static secret, a dynamic secret and using transit.
+
+this is a simple python3 script that: 
+
+* logs into Vault
+* creating a static secret
+* reading a static secret
+* using transit.
+
+## Pre requisites
+
+we are using the HMAC python vault library to facilitate interacting with Vault, for this we will need to install the prerequisites, you can do this with pip by running the following command:
+
+```bash
+pip install -r requirements.txt
+```
+
+this script also expects vault related to the address, token and namespace. you can set these by running the following command:
+
+```bash
+export VAULT_ADDR=http://localhost:8200
+export VAULT_TOKEN=root
+export VAULT_NAMESPACE=example
+```
+
+please note: if not using namespaces,  leave it blank. 
+
+## improvements
+
+I will add different feature branches to this project for different necessities. things like:
+
+* Using different auth methods
+* Using dynamic credentials
+* integrating with different environments like nomad, k8s
+* specific use cases, like Blockchain,Big Data and more
--- main.py
+++ main.py
@@ -1,39 +1,106 @@
 #!/usr/bin/env python
-
+#this is a very basic python code is to serve as an simple reference for future, more robust code
+#hvac is the Hashicorp Vault library we are using, you can find the documentation here: https://hvac.readthedocs.io/en/stable/
 import hvac
 import json
 import requests
 import os
+import pprint
+import base64
+pp = pprint.PrettyPrinter()
 
-VAULT_ADDRESS =  os.environ.get('VAULT_ADDR', None)
-VAULT_TOKEN =  os.environ.get('VAULT_TOKEN', None)
-
+VAULT_ADDR =  os.environ.get('VAULT_ADDR', 'http://localhost:8200')
+VAULT_TOKEN =  os.environ.get('VAULT_TOKEN', 'root')
+VAULT_NAMESPACE = os.environ.get('VAULT_TOKEN', '')
 
 client = hvac.Client(
-    url=VAULT_ADDRESS,
+    url=VAULT_ADDR,
     token=VAULT_TOKEN,
+    namespace=VAULT_NAMESPACE,
     verify=False,
     )
 
-def getKV():
+def createStaticSecret(path,key,value):
+    response = client.secrets.kv.v2.create_or_update_secret(
+    path=path,
+    secret=dict({key:value}),
+    )
+    if (response.status_code == 204):
+        pp.pprint("Static Secret created succesfully")
+    else:    
+        pp.pprint(f"Static Secret not created, something went wrong. response : {response}")
+
+
+def getKV(path,key):
+    read_response = client.secrets.kv.read_secret_version(path=path)
+    secret = read_response['data']['data'][key]
+    pp.pprint(f"Value under path {path}/{key} is {secret}")
+def transitCreateKey(name,convergent_encryption,derived,exportable,allow_plaintext_backup,key_type):
     
+    response = client.secrets.transit.create_key(name,convergent_encryption,derived,exportable,allow_plaintext_backup,key_type)
+    if (response.status_code == 204):
+        pp.pprint("Key created successfully")
+    else:    
+        pp.pprint(f"Key creation failed. response : {response}")
 
-def getPKI():
+def transitEncrypt(key_name,plaintext):
+    # the plaintext parameter is the same as :
+    # ba = plaintext.encode("utf-8")
+    # encodedBytes = base64.urlsafe_b64encode(ba)
+    # encodedStr = str(encodedBytes, "utf-8")
+    encrypt_data_response = client.secrets.transit.encrypt_data(
+    name=key_name,
+    plaintext=str(base64.urlsafe_b64encode(plaintext.encode("utf-8")), "utf-8"),
+)
+    ciphertext = encrypt_data_response['data']['ciphertext']
+    pp.pprint(f"ciphertext: {ciphertext}")
+    return ciphertext
 
-def transitEncrypt():
+def transitDecrypt(key_name,ciphertext):
+    decrypt_data_response = client.secrets.transit.decrypt_data(
+    name=key_name,
+    ciphertext=ciphertext,
+    )
+    unencodedtext=str(base64.urlsafe_b64decode(decrypt_data_response['data']['plaintext'].encode("utf-8")), "utf-8")
+    pp.pprint(f"plaintext: {unencodedtext}")
 
-def transitDecrypt():
+def transitSign(key_name,plaintext):
+    sign_data_response = client.secrets.transit.sign_data(
+    name=key_name,
+    hash_input=str(base64.urlsafe_b64encode(plaintext.encode("utf-8")), "utf-8"),
+    )
+    signature = sign_data_response['data']['signature']
+    pp.pprint(f"Signed Data: {signature}")
+    return signature
 
-def transitSign():
+def transitVerify(key_name,plaintext,signature):
+    verify_signed_data_response = client.secrets.transit.verify_signed_data(
+    name=key_name,
+    hash_input=str(base64.urlsafe_b64encode(plaintext.encode("utf-8")), "utf-8"),
+    signature=signature,
+    )
+    pp.pprint(f"is a valid signature?: {verify_signed_data_response['data']['valid']}")
 
-def transitHMAC():
 
-def transitVerify():
-    
+def transitHMAC(key_name,hash_input, algorithm):
+    generate_hmac_response = client.secrets.transit.generate_hmac(
+    name=key_name,
+    hash_input=str(base64.urlsafe_b64encode(hash_input.encode("utf-8")), "utf-8"),
+    algorithm=algorithm,
+    )
+    pp.pprint(f"HMAC'd data is: {generate_hmac_response['data']['hmac']}")
 
-#Main gets list of entities and creates groups/policies/kv for each entity
+#Main just has the calls to the above methods, comment out as needed
 def main():
-    entity_keys = listEntities()
+   createStaticSecret("Python/Test","Foo","Bar")
+   getKV("Python/Test","Foo")
+   transitCreateKey("example",False,False,True,True,"rsa-2048")
+   ciphertext = transitEncrypt("example","A very secret Secrets that secrets secretly")
+   transitDecrypt("example",ciphertext)
+   signature = transitSign("example","A very secure signature")
+   transitVerify("example","A very secure signature",signature)
+   transitHMAC("example","hashify me","sha2-512")
+
 
 
 if __name__ == '__main__':
